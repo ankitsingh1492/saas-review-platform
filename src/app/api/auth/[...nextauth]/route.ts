f5ba189e3d34373770bce189798ea8dcfe7b2b72
Ankitsingh1492 <Ankitsingh1492@gmail.com>
2025-06-23 20:38:27 +0530

    feat: implement NextAuth authentication with credentials provider, including user signup and session management

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,10 +8,10 @@ import bcrypt from "bcryptjs";
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID!,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
